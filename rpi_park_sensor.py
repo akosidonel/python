ab25b6a646ac186ad3861a03ae30db01fdf3bf7a
akosidonel <reydonelmartinez@gmail.com>
2021-05-25 14:51:07 +0800

    added rpi park sensor file

---
+++ rpi_park_sensor.py
@@ -0,0 +1,27 @@
+#Simple park sensor using Raspberry pi and HC-SR04 Ultrasonic sensor
+
+from gpiozero import DistanceSensor, LED
+from time import sleep
+
+green = LED(17)
+red = LED(22)
+
+
+
+ultrasonic = DistanceSensor(echo=23,trigger=4)
+
+try:
+    while True:
+        ultrasonic.wait_for_in_range()
+        print('Occoupied')
+        green.off()
+        red.on()
+        ultrasonic.wait_for_out_of_range()
+        print('Vaccant')
+        green.on()
+        red.off()
+        
+except KeyboardInterrupt:
+    green.off()
+    red.off()
+    print('Device is turn off...Goodbye')
